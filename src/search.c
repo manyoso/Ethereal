393d5f737e3a5d425e52e7574f87164233fa4cfc
manyoso <manyoso@users.noreply.github.com>
2020-12-29 10:59:57 -0500

    Increase LMR for bad noisy moves.

--- src/search.c
+++ src/search.c
@@ -527,6 +527,9 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
             // Initialize R based on Capture History
             R = MIN(3, 3 - (hist + 4000) / 2000);
 
+            // Increase for bad noisy moves that failed SEE with zero threshold
+            R += movePicker.stage == STAGE_BAD_NOISY;
+
             // Reduce for moves that give check
             R -= !!board->kingAttackers;
 
