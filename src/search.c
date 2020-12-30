ccc3aa793e58277c802e73948189fd209ee489e0
manyoso <manyoso@users.noreply.github.com>
2020-12-30 12:08:42 -0500

    Simplify singular with lmr for quiets.

--- src/search.c
+++ src/search.c
@@ -510,6 +510,9 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
             // Increase for King moves that evade checks
             R += inCheck && pieceType(board->squares[MoveTo(move)]) == KING;
 
+            // Increase if ttMove is singular and this is not it
+            R += singular == 1 && move != ttMove;
+
             // Reduce for moves that give check
             R -= !!board->kingAttackers;
 
