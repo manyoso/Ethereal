2c0767ce3102b7eb06ea7657e01966453ae9ba69
manyoso <manyoso@users.noreply.github.com>
2021-01-04 13:34:57 -0500

    Reduce LMR for recaptures.

--- src/search.c
+++ src/search.c
@@ -534,6 +534,10 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
             // Initialize R based on Capture History
             R = MIN(3, 3 - (hist + 4000) / 2000);
 
+            // Reduce for recaptures of a capture
+            R -= improving && !thread->quietStack[thread->height-1]
+               && MoveTo(bestMove) == MoveTo(thread->moveStack[thread->height-1]);
+
             // Reduce for moves that give check
             R -= !!board->kingAttackers;
 
