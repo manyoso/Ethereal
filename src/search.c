160ccb9a01cce346a3edb9a286c03b67b712b2d8
manyoso <manyoso@users.noreply.github.com>
2021-01-29 20:28:06 -0500

    v2

--- src/search.c
+++ src/search.c
@@ -379,12 +379,12 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
         // Try tactical moves which maintain rBeta
         rBeta = MIN(beta + ProbCutMargin, MATE - MAX_PLY - 1);
 
-        // If we've already done this search don't do it again
+        // If our ttMove already exceeded rBeta, then just return rBeta
         if (   ttHit
             && ttDepth >= depth-4
             && ttValue >= rBeta
             && moveIsTactical(board, ttMove))
-            return ttValue;
+            return rBeta;
 
         initNoisyMovePicker(&movePicker, thread, rBeta - eval);
         while ((move = selectNextMove(&movePicker, board, 1)) != NONE_MOVE) {
